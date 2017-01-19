c77664bd9a15aae5a33177f0e075376b2a2bb10d
徐仲平 <junghsu@xuzhongpingdeMacBook-Pro.local>
2017-01-19 23:13:52 +0800

    update

--- PPVideoImageDemo/PPVideoImage/PPCacheUtil.m
+++ PPVideoImageDemo/PPVideoImage/PPCacheUtil.m
@@ -96,7 +96,8 @@ static id _instance;
 - (NSString *) md5:(NSString *) input {
     const char *cStr = [input UTF8String];
     unsigned char digest[CC_MD5_DIGEST_LENGTH];
-    CC_MD5( cStr, strlen(cStr), digest );
+    CC_MD5( cStr, (CC_LONG)strlen(cStr), digest );
+    
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
     [output appendFormat:@"%02x", digest[i]];
